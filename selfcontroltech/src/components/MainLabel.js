ec7c7675d85dd8dee13434973f7550089548c39d
RSLi <robertsli@163.com>
2017-03-25 18:15:03 -0400

    Add basic label component

---
+++ selfcontroltech/src/components/MainLabel.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+
+export default class MainLable extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hasData:false
+        }
+    }
+
+    render() {
+        const hasData = this.state.hasData;
+        let label = null;
+        if (hasData) {
+            label = (
+                <div>
+                    <h2>Yesterday, at {moment().format('h')}</h2>
+                    <h5>You were doing:</h5>
+                    <h1>Sleeping</h1>
+                </div>
+            );
+        } else {
+            label = (
+                <div>
+                    <h2>No Data yesterday. Enjoy today!</h2>
+                    <h5>or possibly edit history reports :)</h5>
+                </div>
+            );
+        }
+        return (
+            <div>{label}</div>
+        );
+    }
+}
---
+++ selfcontroltech/src/components/MainScreen.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import MainLabel from './MainLabel';
+
+export default class MainScreen extends Component {
+    render() {
+        return <MainLabel/>;
+    }
+}
